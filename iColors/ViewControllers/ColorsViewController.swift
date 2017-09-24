d8d385673a4ad3699b031cb1d9edec2f3044b18f
Sunil Sharma <sunilsharma.ss08@gmail.com>
2017-09-25 00:50:55 +0530

    Color view controller added

--- iColors/ViewControllers/ColorsViewController.swift
+++ iColors/ViewControllers/ColorsViewController.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  ColorsViewController.swift
 //  iColors
 //
 //  Created by Sunil Sharma on 9/25/17.
@@ -8,11 +8,12 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ColorsViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+
+        // Do any additional setup after loading the view.
     }
 
     override func didReceiveMemoryWarning() {
@@ -20,6 +21,4 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-
 }
-
