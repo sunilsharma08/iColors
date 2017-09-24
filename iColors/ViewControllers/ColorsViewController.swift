1e09d1848deffa9273745ff4f2ce22377f8349fe
Sunil Sharma <sunilsharma.ss08@gmail.com>
2017-09-25 01:48:05 +0530

    pan gesture added

--- iColors/ViewControllers/ColorsViewController.swift
+++ iColors/ViewControllers/ColorsViewController.swift
@@ -12,13 +12,52 @@ class ColorsViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        addPanGesture(view: self.view)
+    }
+    
+    func addPanGesture(view:UIView) {
+        let panGestureRecogniser = UIPanGestureRecognizer(target: self, action: #selector(handlePanGesture(sender:)))
+        view.addGestureRecognizer(panGestureRecogniser)
     }
 
+    func handlePanGesture(sender:UIPanGestureRecognizer) {
+        if let senderView = sender.view {
+            let translation = sender.translation(in: senderView)
+            switch sender.state {
+            case .began, .changed:
+                self.view.backgroundColor = UIColor(red: translation.x / 255.0, green: 0.0, blue: translation.y / 255.0, alpha: 1.0)
+            case .ended:
+                break
+            default:
+                break
+            }
+        }
+    }
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
     }
-
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
